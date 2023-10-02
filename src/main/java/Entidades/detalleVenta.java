d01edaa04177a36da2440f0fe371371fb0301709
VictorAngel77 <lx12858@innovaccion.mx>
2023-10-02 17:22:38 -0300

    Creacion de atributos y metodos dedetalleVenta

--- src/main/java/Entidades/detalleVenta.java
+++ src/main/java/Entidades/detalleVenta.java
@@ -9,5 +9,60 @@ package Entidades;
  * @author jdbar
  */
 public class detalleVenta {
-    
+    private int idDetalleVenta;
+    private int cantidad;
+    private Venta venta;
+    private double precioVenta;
+    private Producto producto;
+
+    public detalleVenta(int cantidad, Venta venta, double precioVenta, Producto producto) {
+        this.cantidad = cantidad;
+        this.venta = venta;
+        this.precioVenta = precioVenta;
+        this.producto = producto;
+    }
+
+    public detalleVenta() {
+    }
+
+    public int getIdDetalleVenta() {
+        return idDetalleVenta;
+    }
+
+    public void setIdDetalleVenta(int idDetalleVenta) {
+        this.idDetalleVenta = idDetalleVenta;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
 }
