0120e278866df4ac63f596118a895e4106a02edc
Jose David Bardecio <120067068+Lilded23@users.noreply.github.com>
2023-10-02 16:32:08 -0300

    Create Conexion

---
+++ src/main/java/Conexion/Conexion.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author jdbar
+ */
+public class Conexion {
+    private static Connection conn;
+
+        public static Connection getConnection() {
+        try {
+            conn = DriverManager.getConnection(
+            "jdbc:mariadb://localhost/universidad",
+            "root",
+         "");   
+            
+            System.out.println("Se conecto con exito");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return conn;
+    }
+}
